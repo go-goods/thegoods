6001759aeee3e279043c7d10c3ac3d76f425dfaa
jeff <leterip@me.com>
2012-03-27 11:10:03 -0400

    first working with super ugly page

--- src/app/app.go
+++ src/app/app.go
@@ -8,13 +8,13 @@ import (
 )
 
 type Package struct {
-	Name string
+	Name     string
 	RepoPath string
-	VCS string
+	VCS      string
 }
 
 var (
-	mode          = tmplmgr.Development
+	mode          = tmplmgr.Production
 	assets_dir    = filepath.Join(env("APPROOT", ""), "assets")
 	template_dir  = filepath.Join(env("APPROOT", ""), "templates")
 	base_template = tmplmgr.Parse(tmpl_root("base.tmpl"))
@@ -22,19 +22,35 @@ var (
 	packages = []Package{
 		{"Template Manager", "github.com/goods/tmplmgr", "git"},
 	}
+
+	context = d{
+		"css": []string{
+			"bootstrap.min.css",
+			"bootstrap-responsive.min.css",
+		},
+		"js": []string{
+			"jquery.min.js",
+			"jquery-ui.min.js",
+			"bootstrap.js",
+		},
+		"packages": packages,
+	}
 )
 
-func init() {
-	//set our compiler mode
-	tmplmgr.CompileMode(mode)
+func handle_main(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	if err := base_template.Execute(w, context); err != nil {
+		log.Println(err)
+	}
+}
 
-	//add blocks to base template
+func init() {
+	tmplmgr.CompileMode(mode)
 	base_template.Blocks(tmpl_root("*.block"))
 }
 
 func main() {
-	http.HandleFunc("/", nil)
-
+	http.HandleFunc("/", handle_main)
 	serve_static("/assets", asset_root(""))
 	if err := http.ListenAndServe(":"+env("PORT", "9080"), nil); err != nil {
 		log.Fatal(err)
